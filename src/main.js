33815e1ed91d44689b1f433c58aac0140a765e3e
huangzhiyang <348845730@qq.com>
2017-06-22 19:43:54 +0800

    mm

--- src/main.js
+++ src/main.js
@@ -1,41 +1,3 @@
-<<<<<<< HEAD
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
-
-Vue.config.productionTip = false;
-
-
-Vue.directive("demo",{
-    bind: function (el, binding, vnode) {
-      var s = JSON.stringify
-      el.innerHTML =
-        'name: '       + s(binding.name) + '<br>' +
-        'value: '      + s(binding.value) + '<br>' +
-        'expression: ' + s(binding.expression) + '<br>' +
-        'argument: '   + s(binding.arg) + '<br>' +
-        'modifiers: '  + s(binding.modifiers) + '<br>' +
-        'vnode keys: ' + Object.keys(vnode).join(', ')
-    }
-})
-
-console.log(Vue.util)
-
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  router,
-  data : function(){
-    return{
-      message : "hello!"
-    }
-  },
-  template: '<div><div id="hook-arguments-example" v-demo:hello|type.a.b="message"></div><App/></div>',
-  components: { App }
-})
-=======
 
 // import ElementUI from "element-ui";
 // import "element-ui/lib/theme-default/index.css";
@@ -56,36 +18,39 @@ new Vue({
 
 //index.js
 import Vue from "vue";
-var App = new Vue({
-    el : "#app",
+
+
+
+
+var vm = new Vue({
+    el : "#app",                    //挂载元素
     data : function(){
-        return{
-            message : "hello world"
+        return{                     //数据
+            message : "hello world",
+            a : "test"
         }
     },
+    template : '<div><p>您刚才输入了：{{message}} {{a}}</p><input v-bind:msg="message" type="text" v-model="message"></div>',                  //html模板
+    computed : {},                  //计算属性
+    watch : {},                     //watcher
+    components : {},                //组件体系
+    directives : {},                //自定义指令
+    filters : {},                   //过滤器  
+    mixins : [],                    //方法混入
+    methods : {},                   //实例方法
+
+    //以下为整个vue实例的生命周期函数
     beforeCreate : function(){},    //在实例初始化之后，数据观测(data observer) 和 event/watcher 事件配置之前被调用
-    created : function(){},
-    beforeMount : function(){},
-    mounted : function(){},
-    beforeUpdate : function(){},
-    updated : function(){},
-    activated : function(){},
-    deactivated : function(){},
-    beforeDestroy : function(){},
-    destroyed : function(){},
-    computed : {   //计算属性
-
-    },
-    watch : {      //watcher
-
-    },
-    components : { //子组件体系
-        
-    },
-    methods : {    //实例方法
-        sayHi : function(){
-            console.log("hi");
-        }
-    }
+    created : function(){},         //实例已完数据观测(data observer)，属性和方法的运算，但还没挂载到el元素上
+    beforeMount : function(){},     //挂载到el元素之前
+    mounted : function(){
+        // this.$set(this.$data,"a","test_a");
+        this.a = "test_a";
+    },         //挂载到el元素后，可以访问到真实dom节点了
+    beforeUpdate : function(){},    //数据更新前
+    updated : function(){},         //数据更新完成
+    activated : function(){},       //keep-alive 组件激活时调用
+    deactivated : function(){},     //keep-alive 组件停用时调用
+    beforeDestroy : function(){},   //实例销毁之前调用
+    destroyed : function(){}        //销毁后
 });
->>>>>>> c3959286a3d7eedc08f1cbcb72f6edb0101c58f7
